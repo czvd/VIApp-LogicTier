157ce3521091157eebc77e46c39c859728c3b932
Carolina Valencia Zavidei <zavidei.carolina@gmail.com>
2026-03-20 19:43:07 +0100

    commit

---
+++ Entities/User.cs
@@ -0,0 +1,8 @@
+namespace Entities;
+
+public class User
+{
+    public string Email { get; set; }
+    public string Password { get; set; }  // simple for now
+    public string Role { get; set; }
+}
